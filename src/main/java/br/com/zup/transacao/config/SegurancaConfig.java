6063a8e95aac821e6bdf73c71686af08c2bffc20
Camila Silva <milanasc12@gmail.com>
2020-11-11 17:34:43 -0300

    11.autenticacao_autorizacao

---
+++ src/main/java/br/com/zup/transacao/config/SegurancaConfig.java
@@ -0,0 +1,24 @@
+package br.com.zup.transacao.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+
+import static org.springframework.http.HttpMethod.GET;
+
+@Configuration
+public class SegurancaConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .antMatchers(GET, "/actuator/prometheus").permitAll()
+                .antMatchers(GET, "/transacoes/**").hasAuthority("SCOPE_transacoes:read")
+                .anyRequest().hasAuthority("SCOPE_admin")
+                .and()
+                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    }
+
+}
