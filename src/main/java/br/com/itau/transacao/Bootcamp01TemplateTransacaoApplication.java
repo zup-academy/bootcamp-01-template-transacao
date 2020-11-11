d315961ef6e505d514b4a42309345d6602d4adef
Raphael <raphaelsfreitas94@gmail.com>
2020-11-11 09:21:24 -0300

    Versão 0

---
+++ src/main/java/br/com/itau/transacao/Bootcamp01TemplateTransacaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.transacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Bootcamp01TemplateTransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Bootcamp01TemplateTransacaoApplication.class, args);
+	}
+
+}
