58d150d61f5f1fc1e2701cf21a994e1dda8cc5ca
Matheus Franco <matheus_2222@hotmail.com>
2020-11-13 12:52:55 -0300

    Adicionando segurança a aplicação

---
+++ src/main/java/br/com/cartao/transacao/configuration/SecurityWebConfiguration.java
@@ -0,0 +1,24 @@
+package br.com.cartao.transacao.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@Configuration
+public class SecurityWebConfiguration extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET, "/v1/transacoes/**").hasAuthority("SCOPE_transacao:read")
+                .antMatchers(HttpMethod.POST, "/v1/transacoes/**").hasAuthority("SCOPE_transacao:write")
+                .and()
+                    .csrf().disable()
+                    .httpBasic().disable()
+                    .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                    .oauth2ResourceServer().jwt();
+    }
+}
