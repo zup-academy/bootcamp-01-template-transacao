91c5cdee610bda5b9ac341f9a29f9d376a1bee63
Ariel Alves Dutra <ariel.dutra@zup.com.br>
2020-11-17 09:44:44 -0300

    Adiciona Spring

---
+++ src/main/java/dev/arielalvesdutra/transacao/TransacaoApplication.java
@@ -0,0 +1,12 @@
+package dev.arielalvesdutra.transacao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransacaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransacaoApplication.class, args);
+	}
+}
